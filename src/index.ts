9db1aff3880ecdeaec3c7d9e08811a24f1468ffd
bob1983 <bob.1983.g@gmail.com>
2019-02-13 19:24:17 +0900

    Introduced linter

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import * as express from "express"
+import * as express from 'express'
 
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello Bob'));
+app.get('/', (req, res) => res.send('Hello Bob'))
 
-app.listen(port, () => {});
+app.listen(port, () => {
+  console.log(`Listening localhost:${port}`)
+})
