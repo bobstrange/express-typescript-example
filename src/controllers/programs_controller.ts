465b0d3fa9d891da3c5d146b9bf1fc078955c583
bob1983 <bob.1983.g@gmail.com>
2019-02-20 00:01:26 +0900

    Format

--- src/controllers/programs_controller.ts
+++ src/controllers/programs_controller.ts
@@ -2,7 +2,11 @@ import { Request, Response, NextFunction } from 'express'
 import * as OnsenWrapper from '../apis/onsen'
 
 export const list = (req: Request, res: Response) => {
-  OnsenWrapper.fetchList().then(list => {
-    res.json(list)
-  })
+  OnsenWrapper.fetchList()
+    .then(list => {
+      res.json(list)
+    })
+    .catch(error => {
+      res.json({ error: error })
+    })
 }
