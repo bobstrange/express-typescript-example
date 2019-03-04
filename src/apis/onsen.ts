9fc1787590636680c6cd2bad199b2cbe44fbbd6c
bob1983 <bob.1983.g@gmail.com>
2019-03-04 19:43:11 +0900

    Added show

--- src/apis/onsen.ts
+++ src/apis/onsen.ts
@@ -1,9 +1,22 @@
 import * as Onsen from 'onsen-node'
 
 export const fetchList = () => {
+const getList = (): Promise<object> => {
   return new Promise(resolve => {
     Onsen.getList((list: Array<any>) => {
       resolve(list)
     })
   })
+}
+export const fetch = (programName: string): Promise<object> => {
+  return new Promise(resolve => {
+    Onsen.getInfo(programName, (info: any) => {
+      resolve(info)
+    })
+  })
+}
+
+export const fetchPrograms = async (): Promise<object> => {
+  const list = await getList()
+  return list
 }
--- src/controllers/programs_controller.ts
+++ src/controllers/programs_controller.ts
@@ -1,12 +1,17 @@
 import { Request, Response, NextFunction } from 'express'
 import * as OnsenWrapper from '../apis/onsen'
 
-export const list = (req: Request, res: Response) => {
-  OnsenWrapper.fetchList()
-    .then(list => {
-      res.json(list)
-    })
-    .catch(error => {
-      res.json({ error: error })
-    })
+const errorHandler = (res: Response) => {
+  return (error: any) => res.json({ error })
+}
+
+export const list = async (req: Request, res: Response) => {
+  const list = await OnsenWrapper.fetchPrograms().catch(errorHandler(res))
+  res.json(list)
+}
+
+export const show = async (req: Request, res: Response) => {
+  const name = req.params.name as string;
+  const program = await OnsenWrapper.fetch(name).catch(errorHandler(res))
+  res.json(program)
 }
