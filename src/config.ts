0f23c28a2d982c1a4924bdbfdb882d39f600b0f6
bob1983 <bob.1983.g@gmail.com>
2020-01-25 21:14:18 +0900

    Fixed

--- src/config.ts
+++ src/config.ts
@@ -9,4 +9,4 @@ if (Fs.existsSync('.env')) {
 
 export const NODE_ENV: string = process.env.NODE_ENV as string
 export const MONGODB_URI: string = process.env.MONGODB_URI as string
-export const APP_PORT: number = Number(process.env.APP_PORT as string)
+export const APP_PORT = Number(process.env.APP_PORT as string)
