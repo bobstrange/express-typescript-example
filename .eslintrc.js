25ee57c1ab72fa44523e1fc043d9b62cf947f0f7
bob1983 <bob.1983.g@gmail.com>
2020-01-28 21:28:24 +0900

    Added jest config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,17 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: [
-    '@typescript-eslint'
+    '@typescript-eslint',
+    'jest'
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint'
-  ]
+  ],
+  env: {
+    "jest/globals": true,
+    "node": true
+  }
 }
