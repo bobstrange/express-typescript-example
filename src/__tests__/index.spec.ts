019780996cb3f5079e2115fd585b4205cd101e13
bob1983 <bob.1983.g@gmail.com>
2020-01-28 22:32:32 +0900

    Implementing

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,3 +1,22 @@
-test('basic', () => {
-  expect(10).toBe(10)
+import axios from 'axios'
+
+const LIST_URL = 'http://www.onsen.ag/api/shownMovie/shownMovie.json'
+const SHOW_URL = "http://www.onsen.ag/data/api/getMovieInfo";
+
+const unwrap = (jsonp: string) : any => {
+  const text = jsonp.slice(9, jsonp.length - 3)
+  console.log(text)
+  return JSON.parse(text)
+}
+
+test.skip('list request', async () => {
+  const response = await axios({ method: 'get', url: LIST_URL })
+  console.log(response['data']['result'])
+})
+
+test('show request', async () => {
+  const item = 'toshitai'
+  const response = await axios({ method: 'get', url: `${SHOW_URL}/${item}` })
+  console.log(response['data'])
+  console.log(unwrap(response['data']))
 })
