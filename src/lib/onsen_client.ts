3bd8a74804fb4f22b3e44536b1a5c4452f528497
bob1983 <bob.1983.g@gmail.com>
2020-01-29 21:53:11 +0900

    Added type information

--- src/lib/onsen_client.ts
+++ src/lib/onsen_client.ts
@@ -3,18 +3,57 @@ import axios from 'axios'
 const LIST_URL = 'http://www.onsen.ag/api/shownMovie/shownMovie.json'
 const SHOW_URL = "http://www.onsen.ag/data/api/getMovieInfo";
 
-const unwrapJsonp = (jsonp: string) : any => {
+const unwrapJsonp = <T>(jsonp: string): T => {
   const text = jsonp.slice(9, jsonp.length - 3)
   return JSON.parse(text)
 }
 
+type ShownMovieResult = string[]
+
+interface ShownMovieResponseData {
+  result: ShownMovieResult
+}
+
+type ProgramMoviePath = {
+  pc: string
+  iPhone: string
+  Android: string
+}
+
+type ProgramLink = {
+  imagePath: string
+  url: string
+}
+
+export interface MovieInfoSuccessResult {
+  url: string
+  type: string
+  thumbnailPath: string
+  moviePath: ProgramMoviePath
+  title: string
+  personality: string
+  guest: string
+  update: string
+  schedule: string
+  count: string
+  link: ProgramLink[]
+}
+export interface MovieInfoErrorResult {
+  error: string
+}
+
+export type MovieInfoResult = MovieInfoSuccessResult | MovieInfoErrorResult
+
+type MovieInfoResponseData = string
+
 export class OnsenClient {
-  static async fetchShownMovie(): Promise<string[]> {
-    const response = await axios.get(LIST_URL)
+  static async fetchShownMovie(): Promise<ShownMovieResult> {
+    const response = await axios.get<ShownMovieResponseData>(LIST_URL)
     return response.data.result
   }
-  static async fetchMovieInfo(movieName: string): Promise<any> {
-    const response = await axios.get(`${SHOW_URL}/${movieName}`)
-    return unwrapJsonp(response.data)
+  static async fetchMovieInfo(movieName: string): Promise<MovieInfoResult> {
+    const response = await axios.get<MovieInfoResponseData>(`${SHOW_URL}/${movieName}`)
+    console.log(response)
+    return unwrapJsonp<MovieInfoResult>(response.data)
   }
 }
