0463c4b02fec9692661f83fa94d6fc26478fcd9f
bob1983 <bob.1983.g@gmail.com>
2020-01-30 22:27:34 +0900

    Added fetchProgram implementation

--- src/__tests__/onsen_repository.spec.ts
+++ src/__tests__/onsen_repository.spec.ts
@@ -1,3 +1,4 @@
+
 import Sinon from 'sinon'
 
 import {
@@ -16,14 +17,82 @@ describe('OnsenRepository', () => {
       return Promise.resolve(['foo', 'bar'])
     },
     fetchMovieInfo() {
-      return Promise.resolve({} as  MovieInfoResult)
+      return Promise.resolve({
+        type: 'sound',
+        thumbnailPath: '/program/toshitai/image/327_pgi05_m.jpg',
+        moviePath: {
+          pc: 'https://onsen-dl.sslcs.cdngc.net/radio/toshitai200128Tu7R.mp3',
+          iPhone:
+            'https://onsen-dl.sslcs.cdngc.net/radio/toshitai200128Tu7R.mp3',
+          Android:
+            'https://onsen-dl.sslcs.cdngc.net/radio/toshitai200128Tu7R.mp3'
+        },
+        title: 'セブン-イレブン presents 佐倉としたい大西',
+        personality: '佐倉綾音 / 大西沙織',
+        guest: '',
+        update: '2020.1.28',
+        count: '200',
+        schedule:
+          '毎週火曜配信 / 超！A＆G＋：23時30分～24時（動画配信） ＜音泉＞：24時～（音声配信）＋音声ダケの特別コーナー有',
+        optionText: '',
+        mail: 'toshitai@joqr.net',
+        copyright: '©文化放送 ©Internet Radio Station＜音泉＞',
+        url: 'toshitai',
+        link: [
+          {
+            imagePath: '/program/toshitai/image/327_pgl01.jpg',
+            url: 'http://www.agqr.jp/'
+          }
+        ],
+        recommendGoods: [],
+        recommendMovie: [
+          {
+            imagePath: '/program/saekano/image/201_pgi03_b.jpg',
+            url: '/program/saekano/'
+          }
+        ],
+        cm: [],
+        allowExpand: 'false'
+      } as MovieInfoResult);
     }
   }
 
-  test('fetchProgramNames', async () => {
+  beforeEach(() => {
     repository = new OnsenRepository(clientStub)
-    expect(await repository.fetchProgramNames()).toEqual(
-      ['foo', 'bar']
-    )
+  })
+
+  describe('fetchProgramNames', () => {
+    test('returns program names', async () => {
+      expect(await repository.fetchProgramNames()).toEqual(
+        ['foo', 'bar']
+      )
+    })
+  })
+
+  describe('fetchProgram', () => {
+    test('returns a Program', async () => {
+      const program = await repository.fetchProgram('toshitai')
+      expect(program.name).toBe('toshitai')
+      expect(program.type).toBe('sound')
+      expect(program.thumbnailPath).toBe('/program/toshitai/image/327_pgi05_m.jpg')
+      expect(program.moviePath).toEqual({
+        pc: 'https://onsen-dl.sslcs.cdngc.net/radio/toshitai200128Tu7R.mp3',
+        iPhone:
+          'https://onsen-dl.sslcs.cdngc.net/radio/toshitai200128Tu7R.mp3',
+        Android:
+          'https://onsen-dl.sslcs.cdngc.net/radio/toshitai200128Tu7R.mp3'
+      })
+      expect(program.title).toBe('セブン-イレブン presents 佐倉としたい大西')
+      expect(program.personalities).toEqual(['佐倉綾音', '大西沙織'])
+      expect(program.guest).toBe('')
+      expect(program.update).toEqual(new Date(2020, 1, 28))
+      expect(program.count).toBe(200)
+      expect(program.links).toEqual([
+        {
+          imagePath: '/program/toshitai/image/327_pgl01.jpg',
+          url: 'http://www.agqr.jp/'
+        }
+      ]);
+    })
   })
 })
--- src/onsen_repository.ts
+++ src/onsen_repository.ts
@@ -1,4 +1,8 @@
-import { OnsenClient, buildOnsenClient } from './lib/onsen_client'
+import {
+  OnsenClient,
+  buildOnsenClient,
+  MovieInfoSuccessResult
+} from "./lib/onsen_client";
 
 type ProgramMoviePath = {
   pc: string
@@ -26,42 +30,63 @@ class Program {
   ) {}
 }
 
-// class ProgramFactory {
-//   static buildFromShowApiResponse(response: ShowApiSuccessResponse): Program {
-//     const [year, month, day] = response.update.split('.').map(_ => Number(_))
-//     const personalities = response.personality.split('/').map(_ => _.trim())
+const buildProgramFromShowApiResponse = (
+  response: MovieInfoSuccessResult
+): Program => {
+  const [year, month, day] = response.update.split(".").map(_ => Number(_));
+  const personalities = response.personality.split("/").map(_ => _.trim());
 
-//     return new Program(
-//       response.url,
-//       response.type,
-//       response.thumbnailPath,
-//       response.moviePath,
-//       response.title,
-//       personalities,
-//       response.guest,
-//       new Date(year, month, day),
-//       Number(response.count),
-//       response.link
-//     )
-//   }
-// }
+  return new Program(
+    response.url,
+    response.type,
+    response.thumbnailPath,
+    response.moviePath,
+    response.title,
+    personalities,
+    response.guest,
+    new Date(year, month, day),
+    Number(response.count),
+    response.link
+  );
+};
+
+const buildNullProgram = (
+  name: string
+): Program => {
+  return new Program(
+    name,
+    '',
+    '',
+    { pc: '', iPhone: '', Android: '' },
+    '',
+    [],
+    '',
+    new Date(),
+    0,
+    []
+  )
+}
+
 interface OnsenRepositoryInterface {
   fetchProgramNames(): Promise<string[]>
-  // fetchProgram(name: string): Promise<Program>
+  fetchProgram(name: string): Promise<Program>
 }
 
 export class OnsenRepository implements OnsenRepositoryInterface {
-  constructor(private client: OnsenClient) {
-  }
+  constructor(private client: OnsenClient) {}
 
   async fetchProgramNames(): Promise<string[]> {
     return await this.client.fetchShownMovie()
   }
 
-  // async fetchProgram(name: string): Promise<Program> {
-  //   const result = await OnsenClient.fetchMovieInfo(name)
-
-  // }
+  async fetchProgram(name: string): Promise<Program> {
+    const response = await this.client.fetchMovieInfo(name)
+    if ('error' in response) {
+      return buildNullProgram(name)
+    } else {
+      return buildProgramFromShowApiResponse(response)
+    }
+  }
 }
 
 export const buildOnsenRepository = (): OnsenRepository => {
