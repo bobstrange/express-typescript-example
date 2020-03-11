0ca2db48c590a89825fea7a767b01d5dbf158f98
bob1983 <bob.1983.g@gmail.com>
2020-03-11 20:20:35 +0900

    Remove unused

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProgramsModule } from './programs/programs.module';
 
 @Module({
-  imports: [ProgramsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProgramsModule]
 })
 export class AppModule {}
