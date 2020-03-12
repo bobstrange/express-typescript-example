1527bbf440c7ebf23e49c4464883948fa91f9f5f
bob1983 <bob.1983.g@gmail.com>
2020-03-12 19:55:22 +0900

    Use ormconfig.ts

--- api/src/db/migrations/1584007729335-CreateProgram.ts
+++ api/src/db/migrations/1584007729335-CreateProgram.ts
@@ -1,8 +1,32 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateProgram1584007729335 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
+      const table = new Table({
+        name: 'program',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true
+          },
+          {
+            name: 'name',
+            type: 'char',
+          },
+          {
+            name: 'title',
+            type: 'char'
+          },
+          {
+            name: 'updateAt',
+            type: 'timestamp'
+          }
+        ]
+      })
+      queryRunner.createTable(table, true)
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
